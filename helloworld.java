a47b27b183a8f17b255156a03ffd184cf5a3d472
mohammedelsharani <111609857+mohammedelsharani@users.noreply.github.com>
2023-01-17 20:59:22 +0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Jan 3rd - 08:30 AM - triggered by webhook2");
+    System.out.println("Hello World - Jan 5rd - 08:56 AM ");
     }
 }
