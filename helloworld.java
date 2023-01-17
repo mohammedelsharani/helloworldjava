e8652a54b64b51f81ce7288103ba41e4197b693f
mohammedelsharani <111609857+mohammedelsharani@users.noreply.github.com>
2023-01-17 22:56:11 +0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Jan 17th - 09:13 AM ");
+    System.out.println("Hello World - Jan 17th - 10:52 PM ");
     }
 }
